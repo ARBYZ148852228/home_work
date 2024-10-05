9744d1c2f74c91044474dec8f2490bc44a5b9476
ARBYZ148852228 <anonim15255@gmail.com>
2024-10-05 18:39:33 +0300

    11111

---
+++ cycles.py
@@ -0,0 +1,16 @@
+# for i in range(5):#     print('Python')
+
+# for i in 'Python':#     print(i)
+
+#for number in range(1, 6):#   print(number)
+
+#a = 1#while a < 10:
+#    print('Цикл выполнился', a, 'раз(а)')#    a = a+1
+#print('Цикл окончен')
+
+#a = 1#while a < 5:
+#   print('условие верно')#   a = a + 1
+#else:#   print('условие неверно')
+
+#n = int(input())#while n != 0:
+#     print(n + 5)#     n = int(input())
